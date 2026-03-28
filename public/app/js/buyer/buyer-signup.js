07784f9cb1b2fb515692ada23c20949506fade12
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-03-28 01:13:43 -0400

    Update buyer-signup.js

--- public/app/js/buyer/buyer-signup.js
+++ public/app/js/buyer/buyer-signup.js
@@ -1,5 +1,4 @@
-// buyer-signup.js
-import API from "./api.js";
+import API from "../api.js";
 
 const form = document.getElementById("buyer-signup-form");
 
@@ -7,20 +6,13 @@ form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const fd = new FormData(form);
-  const payload = {
+
+  const user = await API.post("/api/signup", {
     role: "buyer",
     name: fd.get("name"),
     email: fd.get("email"),
     password: fd.get("password")
-  };
+  });
 
-  try {
-    const user = await API.post("/api/signup", payload);
-    window.location.href = `/pages/buyer-dashboard.html?user=${encodeURIComponent(
-      user.id
-    )}`;
-  } catch (err) {
-    console.error(err);
-    alert("There was an issue creating your buyer account.");
-  }
+  window.location.href = `/pages/buyer/buyer-dashboard.html?user=${user.id}`;
 });
