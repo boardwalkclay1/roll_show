3d01fa022b6103cf9a8b095c8406c544c2c41fa9
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-01 13:14:54 -0400

    Create 0022_feed_likes.sql

---
+++ migrations/0022_feed_likes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE feed_likes (
+  id TEXT PRIMARY KEY,
+  post_id TEXT NOT NULL,
+  user_id TEXT NOT NULL,
+  created_at TEXT NOT NULL,
+
+  FOREIGN KEY (post_id) REFERENCES feed_posts(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
