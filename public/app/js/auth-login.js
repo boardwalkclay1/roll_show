46c0395703bcdc2e4b683034449af8c134dc9a2f
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-06 02:21:23 -0400

    Update auth-login.js

--- public/app/js/auth-login.js
+++ public/app/js/auth-login.js
@@ -34,7 +34,7 @@ if (form) {
     let target = "/";
 
     if (session.is_owner) {
-      target = "/pages/owner/owner-dashboard.htmll";
+      target = "/pages/owner/owner-dashboard.html";
     } else {
       switch (session.role) {
         case "skater":
