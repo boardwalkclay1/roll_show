4a118afd317955594c4326af9ab784511f9aa91c
Your Name <boardwalkclay1@gmail.com>
2026-04-17 13:20:38 -0400

    Remove all JS files

--- public/app/js/buyer/ticket-confirmation.js
+++
@@ -1,7 +0,0 @@
-import { getUserIdFromQuery } from "../utils.js";
-
-const params = new URLSearchParams(window.location.search);
-const showTitle = params.get("show");
-const message = document.getElementById("confirmation-message");
-
-message.textContent = `Your ticket for "${showTitle}" is confirmed.`;
--- public/app/js/utils.js
+++
@@ -1,5 +0,0 @@
-// /app/js/utils.js
-export function getUserIdFromQuery() {
-  const params = new URLSearchParams(window.location.search);
-  return params.get("user") || "";
-}
